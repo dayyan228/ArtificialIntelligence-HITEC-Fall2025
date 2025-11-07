5ae94ca3d39b9df00c4f2e45eefbbf84b77b177b
Mubashir Iqbal <mubshr07@gamil.com>
2025-11-07 21:43:56 +0500

    uploading to Github

---
+++ 03 - Lab-Manual-Codes/LabManual_1.py
@@ -0,0 +1,17 @@
+print("\n ------------------ \n")
+
+name = ["Mubashir Iqbal", "HITEC University", "Cyber Security", "Quiz", "Assignments", "Mid-Term", "Final-Term", "Result"]
+
+print(name[2:5])
+print(type(name[2]))
+print(type(name))
+print(len(name))
+print("\n ------------------ \n")
+
+
+for i in range(len(name)):
+    print(f"{i} -> {name[i]}")  
+
+
+
+print("\n ------------------ \n")
